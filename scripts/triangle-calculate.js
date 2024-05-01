a1da79e8e538f747ce07eb1a75be3262147d12d3
swadeshchisim <schisim195@gmail.com>
2024-05-01 22:42:20 +0600

    triangle functional js code added

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,24 @@
 function calculateTriangleArea() {
-    console.log('inside function');
+    /* get triangle base */
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    // console.log(typeof base);
+
+
+    // get triangle height value
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    // console.log(height);
+
+    //calculate triangle area
+    const area = 0.5 * base * height;
+    // console.log(area);
+
+    let areaText = document.getElementById('triangle-area');
+    areaText.innerText = area;
+
+    triangleBaseInput.value = '';
+    triangleHeightInput.value = '';
 }
