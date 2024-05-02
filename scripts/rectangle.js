fcd5bdd5be74825689c171001b22fe0e6a49100a
swadeshchisim <schisim195@gmail.com>
2024-05-02 23:59:24 +0600

    javascript functionality added for rectangle area

--- scripts/rectangle.js
+++ scripts/rectangle.js
@@ -1,3 +1,23 @@
 function calculateRectangleArea() {
+    //get width of the rectangle
+    const widthInput = document.getElementById('rectangle-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+
+    // get legnth of the rectangle
+    const lengthInput = document.getElementById('rectangle-length');
+    const lengthText = lengthInput.value;
+    const length = parseFloat(lengthText);
     
+
+    // calculate rectangle area
+    const area = width * length;
+    
+
+    //display rectangle area
+    const reactangleAreaSpan = document.getElementById('rectangle-area');
+    reactangleAreaSpan.innerText = area;
+
+    widthInput.value = '';
+    lengthInput.value = '';
 }
--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -3,18 +3,16 @@ function calculateTriangleArea() {
     const triangleBaseInput = document.getElementById('triangle-base');
     const triangleBaseText = triangleBaseInput.value;
     const base = parseFloat(triangleBaseText);
-    // console.log(typeof base);
 
 
     // get triangle height value
     const triangleHeightInput = document.getElementById('triangle-height');
     const triangleHeightText = triangleHeightInput.value;
     const height = parseFloat(triangleHeightText);
-    // console.log(height);
 
     //calculate triangle area
     const area = 0.5 * base * height;
-    // console.log(area);
+
 
     let areaText = document.getElementById('triangle-area');
     areaText.innerText = area;
