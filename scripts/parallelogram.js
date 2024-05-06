6361802f897b6a58e6cfa8f6222672b9f7765b12
swadeshchisim <schisim195@gmail.com>
2024-05-06 17:12:29 +0600

    parallelogram code added to area calculation

--- scripts/parallelogram.js
+++ scripts/parallelogram.js
@@ -1,25 +1,10 @@
 function calculateParallelogramArea() {
     const base = getInputValueById('parallelogram-base');
-
     const height = getInputValueById('parallelogram-height');
-
     //calculate area
     const area = base * height;
-
     setInnerTextById('parallelogram-area', area);
-}
 
-//get input by id
-
-function getInputValueById(inputFieldId) {
-    const inputField = document.getElementById(inputFieldId);
-    const inputValueText = inputField.value;
-    const inputValue = parseFloat(inputValueText);
-    // console.log(inputValue);
-    return inputValue;
-}
-
-function setInnerTextById(elementId, area) {
-    const element = document.getElementById(elementId);
-    element.innerText = area;
+    const calculationArea = document.getElementById('area-calculation');
+    calculationArea.innerText = 'Parallelogram Area: ' + area;
 }
